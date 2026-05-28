e5f01ab2cf2d99a924cbcb6e6d3fa8d06ba45a96
N8k8t66 <nikita.nunizyamov@yandex.ru>
2026-05-28 05:36:36 +0400

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DatabaseApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DatabaseApp")]
+[assembly: AssemblyCopyright("© 2026")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a45e2af-2109-4131-8bbc-4602a7f6f6a5")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
